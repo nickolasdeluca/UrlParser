27dd803a6c242de569c133884793fe8a85820fd4
Nickolas Deluca <nickolasdeluca@live.com>
2021-03-12 14:33:17 -0300

    Primeira versão oficial

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# UrlParser
- Construtor de URL para Delphi
+# UrlParser for Delphi
+ Node.js like URL constructor for Delphi.
+
+## How to use it
+
+Add it to your uses
+
+```pascal
+uses UrlParser;
+```
+Then you can simply call it.
+
+```pascal
+var
+	url: String;
+begin
+	url := TUrlParser.New
+				.BaseUrl('https://www.thisisatest.com/tests')
+				.AddParameter('thisisa', 'test')
+				.AddParameter('useiton', 'delphi')
+				.ToString;
+end;
+```
+
+The sample above would return the url below as a string
+
+`
+https://www.thisisatest.com/tests?thisisa=test&useiton=delphi
+`
+
+### How to install it
+
+##### Install it manually:
+
+Simply drop the UrlParser.pas in your project's folder and add it to the project.
+
+##### Installation using [**Boss**](https://github.com/HashLoad/boss):
+
+```
+boss install github.com/nickolasdeluca/UrlParser
+```
---
+++ src/UrlParser.pas
@@ -0,0 +1,62 @@
+unit UrlParser;
+
+interface
+
+Uses
+  { System }
+  System.StrUtils, System.Generics.Collections, System.Classes, System.SysUtils;
+
+type
+  TUrlParser = class
+
+  private
+    FBaseUrl: String;
+    FParams: TStringlist;
+  public
+    class function New: TUrlParser;
+    function BaseUrl(AUrl: String): TUrlParser;
+    function AddParameter(AName: String; AValue: String): TUrlParser;
+    function ToString: String; override;
+  end;
+
+implementation
+
+{ TUrlParser }
+
+class function TUrlParser.New: TUrlParser;
+begin
+  Result := TUrlParser.Create;
+  Result.FBaseUrl := '';
+  Result.FParams := TStringlist.Create;
+end;
+
+function TUrlParser.AddParameter(AName: String; AValue: String): TUrlParser;
+begin
+  Result := Self;
+  Result.FParams.AddPair(AName, AValue);
+end;
+
+function TUrlParser.BaseUrl(AUrl: String): TUrlParser;
+begin
+  Result := Self;
+  FBaseUrl := AUrl;
+end;
+
+function TUrlParser.ToString: String;
+var
+  AParam: String;
+begin
+  Result := FBaseUrl;
+
+  if FParams.Count > 0 then
+  begin
+    for AParam in FParams do
+    begin
+      Result := Result + IfThen(Result.Contains('?'), '&', '?') + AParam;
+    end;
+  end;
+  
+  Self.Destroy;
+end;
+
+end.
